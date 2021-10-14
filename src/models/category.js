a8e1720009d6e0e08006aeb70b79efab7b2c30da
Zion <zionman@verizon.net>
2021-10-14 16:20:22 -0500

    Add models for linting

---
+++ src/models/category.js
@@ -0,0 +1,10 @@
+class Category {
+	constructor(id, categoryName, description, productList) {
+		this.id = id;
+		this.categoryName = categoryName;
+		this.description = description;
+		this.productList = productList;
+	}
+}
+
+export default Category;
---
+++ src/models/product.js
@@ -0,0 +1,11 @@
+class Product {
+	constructor(id, productName, supplier, category, unitPrice) {
+		this.id = id;
+		this.productName = productName;
+		this.supplier = supplier;
+		this.category = category;
+		this.unitPrice = unitPrice;
+	}
+}
+
+export default Product;
