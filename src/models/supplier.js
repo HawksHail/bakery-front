70a20e2fd730a29ff5fd71cef1e769ae42b7a1a5
Zion <zionman@verizon.net>
2021-10-15 16:15:52 -0500

    added supplier model

---
+++ src/models/supplier.js
@@ -0,0 +1,10 @@
+class Supplier {
+	constructor(id, companyName, contactName, productList) {
+		this.id = id;
+		this.companyName = companyName;
+		this.contactName = contactName;
+		this.productList = productList;
+	}
+}
+
+export default Supplier;
