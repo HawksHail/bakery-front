05798fb6b32af2880c94da11a518a9d63995ecd4
Zion <zionman@verizon.net>
2021-10-20 16:02:34 -0500

    adjust spacing

--- src/components/DisplayProduct.jsx
+++ src/components/DisplayProduct.jsx
@@ -5,7 +5,7 @@ import Product from "../models/product";
 function DisplayProduct(props) {
 	return (
 		<div className="col-6 col-sm-4 col-md-3 col-lg-2 p-1">
-			<div className="card m-1" data-testid="card">
+			<div className="card m-2" data-testid="card">
 				{/* <img
 					className="card-img-top img-fluid p-5 p-sm-2"
 					src="https://m.media-amazon.com/images/I/61KB6fUhcSL._AC_SL1500_.jpg"
