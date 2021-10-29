191f5b436a28a51bb8d796ba36a059f8f3046817
Zion <zionman@verizon.net>
2021-10-29 12:12:41 -0500

    Basic auth

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,24 +3,27 @@ import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import { useAuth0 } from "@auth0/auth0-react";
+
 import "../styles/Navbar.css";
 import logo from "../logo.svg";
+import LogoutButton from "./LogoutButton";
+import LoginButton from "./LoginButton";
 
 function DisplayNavbar() {
+	const { user, isAuthenticated } = useAuth0();
+
 	return (
-		<Navbar
-			bg="dark"
-			variant="dark"
-			expand="sm"
-			sticky="top"
-			className="justify-content-between"
-		>
+		<Navbar bg="dark" variant="dark" expand="sm" sticky="top">
 			<Container fluid>
 				<Navbar.Brand as={Link} to="/">
 					<img className="brand" src={logo} alt="logo" />
 				</Navbar.Brand>
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
-				<Navbar.Collapse id="basic-navbar-nav">
+				<Navbar.Collapse
+					id="basic-navbar-nav"
+					className="justify-content-between"
+				>
 					<Nav className="me-auto mb-2 mb-lg-0">
 						<Nav.Link as={Link} to="/">
 							Home
@@ -34,6 +37,21 @@ function DisplayNavbar() {
 						<Nav.Link as={Link} to="/cart">
 							Cart
 						</Nav.Link>
+					</Nav>
+					<Nav>
+						{isAuthenticated && (
+							<div>
+								<LogoutButton />
+								<img
+									src={user.picture}
+									alt={user.name}
+									className="rounded-circle mx-2"
+									height="40px"
+									width="40px"
+								/>
+							</div>
+						)}
+						{!isAuthenticated && <LoginButton />}
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./styles/index.css";
@@ -8,11 +9,17 @@ import reportWebVitals from "./reportWebVitals";
 import ProductsContextProvider from "./contexts/ProductsContextProvider";
 
 ReactDOM.render(
-	<React.StrictMode>
-		<ProductsContextProvider>
-			<App />
-		</ProductsContextProvider>
-	</React.StrictMode>,
+	<Auth0Provider
+		domain="dev-1en1mmfn.us.auth0.com"
+		clientId="EL35E8d1cWm7zpI61PhIG8ubgsj8xx6O"
+		redirectUri={window.location.origin}
+	>
+		<React.StrictMode>
+			<ProductsContextProvider>
+				<App />
+			</ProductsContextProvider>
+		</React.StrictMode>
+	</Auth0Provider>,
 	document.getElementById("root")
 );
 // If you want to start measuring performance in your app, pass a function
