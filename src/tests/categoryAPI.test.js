19aa5b292d1a3d4c258a736df4852914b8f88f8a
Zion <zionman@verizon.net>
2021-11-02 11:11:15 -0500

    api tests

---
+++ src/tests/categoryAPI.test.js
@@ -0,0 +1,23 @@
+import { getAllCategories, getCategory } from "../api/categoryAPI";
+import { url } from "../api/url";
+
+let fetchSpy;
+beforeEach(() => {
+	fetchSpy = jest.spyOn(global, "fetch").mockImplementation(() =>
+		Promise.resolve({
+			json: () => Promise.resolve({}),
+		})
+	);
+});
+
+test("getAllCategories fetches properly", () => {
+	getAllCategories();
+
+	expect(fetchSpy).toBeCalledWith(`${url}/category`);
+});
+
+test("getCategory fetches properly", () => {
+	getCategory(1234);
+
+	expect(fetchSpy).toBeCalledWith(`${url}/category/1234`);
+});
