695b2696c12241af8b976d64c5399d5dc7cd74db
Zion <zionman@verizon.net>
2021-11-02 11:35:45 -0500

    change url to public endpoint

--- src/api/url.js
+++ src/api/url.js
@@ -1,4 +1,4 @@
 export const url =
 	// "http://zionmanteyprojectbackend-env.eba-sfdxaxrh.us-east-2.elasticbeanstalk.com/";
-	"http://localhost:5000";
-// "https://zion.ee-cognizantacademy.com";
+	// "http://localhost:5000";
+	"https://zion.ee-cognizantacademy.com";
