4dca193aec19f37785a28a7de03d312a4a51a706
Zion <zionman@verizon.net>
2021-11-05 10:54:47 -0500

    add image url to product model

--- src/models/product.js
+++ src/models/product.js
@@ -1,10 +1,11 @@
 class Product {
-	constructor(id, productName, supplier, category, unitPrice) {
+	constructor(id, productName, supplier, category, unitPrice, imgURL) {
 		this.id = id;
 		this.productName = productName;
 		this.supplier = supplier;
 		this.category = category;
 		this.unitPrice = unitPrice;
+		this.imgURL = imgURL;
 	}
 }
 
