068b21f41f7b2a0d693cb4f8f06b909e7c13c80b
Zion <zionman@verizon.net>
2021-11-05 10:48:44 -0500

    remove unneeded data-testid

--- src/components/DisplayProduct.jsx
+++ src/components/DisplayProduct.jsx
@@ -11,31 +11,28 @@ function DisplayProduct(props) {
 
 	return (
 		<Col xs={6} sm={4} md={3} lg={2} className="p-1">
-			<Card className="m-2 h-100" data-testid="productCard">
+			<Card className="m-2 h-100">
 				<Card.Body className="d-flex align-items-start flex-column">
 					<img
 						className="card-img-top img-fluid rounded-3 mb-1"
 						src={`https://picsum.photos/400/400?random=${props.product.id}`}
 						alt="{props.product.productName} image"
 					/>
-					<Card.Title data-testid="productName">
-						{props.product.productName}
-					</Card.Title>
-					<Card.Subtitle data-testid="categoryName">
+					<Card.Title>{props.product.productName}</Card.Title>
+					<Card.Subtitle>
 						{props.categoryName ||
 							props.product.category.categoryName}
 					</Card.Subtitle>
-					<Card.Text data-testid="companyName">
+					<Card.Text>
 						Sold by: {props.product.supplier.companyName}
 					</Card.Text>
-					<Card.Text data-testid="unitPrice" className="mb-0">
+					<Card.Text className="mb-0">
 						${props.product.unitPrice}
 					</Card.Text>
 
 					<Button
 						className="w-100 mt-auto d-flex justify-content-around"
 						onClick={buttonClick}
-						data-testid="productCardButton"
 					>
 						{props.buttonText}
 						{props.quantity ? (
