ecc3345cbc83b0f99f98fe1aa53da26188f6dc9b
Zion <zionman@verizon.net>
2021-11-15 12:28:53 -0600

    Login page redirect to previous page on auth0 load

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { withRouter } from "react-router";
+import { Spinner } from "react-bootstrap";
+import PropTypes from "prop-types";
 
-function Login() {
-	return <h2>You must log in to do that!</h2>;
+function Login(props) {
+	const { isAuthenticated, isLoading } = useAuth0();
+
+	useEffect(() => {
+		if (!isLoading && isAuthenticated) {
+			if (props.location.state?.from) {
+				props.history.push(props.location.state.from);
+			} else {
+				props.history.push("/");
+			}
+		}
+	}, [isAuthenticated, isLoading]);
+
+	return (
+		<h2>
+			{isLoading ? (
+				<Spinner animation="border" role="status">
+					<span className="visually-hidden">Loading...</span>
+				</Spinner>
+			) : (
+				"You must log in to do that!"
+			)}
+		</h2>
+	);
 }
 
-export default Login;
+Login.propTypes = {
+	props: PropTypes.object,
+	history: PropTypes.object,
+	location: PropTypes.object,
+};
+
+export default withRouter(Login);
