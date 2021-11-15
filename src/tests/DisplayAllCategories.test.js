276f7c9b3afa17d8d6f1fc2c87c21ce9a6c2481e
Zion <zionman@verizon.net>
2021-11-15 10:30:39 -0600

    refactored with nock

--- src/tests/DisplayAllCategories.test.js
+++ src/tests/DisplayAllCategories.test.js
@@ -1,26 +1,24 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
+import nock from "nock";
 
 import AppContext from "../contexts";
 import DisplayAllCategories from "../components/DisplayAllCategories";
 import Category from "../models/category";
 import { url } from "../api/url";
 
-const fakeCategories = {
-	categories: [
-		new Category(1, "name1", "description1", [1]),
-		new Category(4, "name2", "description2", [2]),
-	],
-};
+const categories = [
+	new Category(1, "name1", "description1", [1]),
+	new Category(4, "name2", "description2", [2]),
+];
 
-let fetchSpy;
-beforeEach(() => {
-	fetchSpy = jest.spyOn(global, "fetch").mockImplementation(() =>
-		Promise.resolve({
-			json: () => Promise.resolve(fakeCategories),
-		})
-	);
+afterEach(function () {
+	if (!nock.isDone()) {
+		console.log(`nock.pendingMocks()`, nock.pendingMocks());
+		nock.cleanAll();
+		throw new Error("Not all nock interceptors were used!");
+	}
 });
 
 test("list not loaded yet", () => {
@@ -35,16 +33,27 @@ test("list not loaded yet", () => {
 	expect(screen.getByText(/Loading/)).toBeInTheDocument();
 });
 
-test("API is called and all products are rendered", () => {
+test("API is called and all products are rendered", async () => {
+	const scope = nock(url)
+		.defaultReplyHeaders({
+			"Access-Control-Allow-Origin": "*",
+			"Access-Control-Allow-Headers": "Authorization",
+		})
+		.get("/category")
+		.reply(200, categories);
+
+	const setCategories = jest.fn();
 	render(
-		<AppContext.Provider value={fakeCategories}>
+		<AppContext.Provider value={{ categories, setCategories }}>
 			<Router>
 				<DisplayAllCategories />
 			</Router>
 		</AppContext.Provider>
 	);
 
-	expect(fetchSpy).toBeCalledWith(`${url}/category`);
+	await waitFor(() => {
+		expect(setCategories).toBeCalledTimes(1);
+	});
 
 	const cards = screen.getAllByText(/name[0-9]/);
 	expect(cards.length).toBe(2);
