bff1f35baf345cfa3954e30ede74f32980d91494
Zion <zionman@verizon.net>
2021-11-16 15:23:01 -0600

    PrivateRoute test

---
+++ src/tests/PrivateRoute.test.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import { useAuth0 } from "@auth0/auth0-react";
+
+import PrivateRoute from "../components/PrivateRoute";
+
+jest.mock("@auth0/auth0-react");
+
+test("Authenticated renders children", () => {
+	useAuth0.mockReturnValue({
+		isAuthenticated: true,
+	});
+
+	render(
+		<Router>
+			<PrivateRoute>
+				<p>protected route</p>
+			</PrivateRoute>
+		</Router>
+	);
+
+	expect(screen.getByText("protected route")).toBeInTheDocument();
+});
+
+test("Unauthenticated redirects", () => {
+	useAuth0.mockReturnValue({
+		isAuthenticated: false,
+	});
+
+	render(
+		<Router>
+			<PrivateRoute path="/protected">
+				<p>protected route</p>
+			</PrivateRoute>
+		</Router>
+	);
+
+	expect(screen.queryByText("protected route")).not.toBeInTheDocument();
+});
