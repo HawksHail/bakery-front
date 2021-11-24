2df718dcef3729b2e23736bbfa20071f2763cab3
Zion <zionman@verizon.net>
2021-11-24 16:57:22 -0600

    Basic order table
    Table of orders with order number, date, and number of items in order

--- src/page/OrdersPage.jsx
+++ src/page/OrdersPage.jsx
@@ -1,10 +1,27 @@
 /* eslint-disable no-unused-vars */ //todo remove
 import React from "react";
+import { Table } from "react-bootstrap";
 
 function OrderPage() {
 	return (
 		<>
 			<h1>Orders</h1>
+			<Table striped bordered hover>
+				<thead>
+					<tr>
+						<th>Order Number</th>
+						<th>Order Date</th>
+						<th>Number of Items</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>Lorem, ipsum.</td>
+						<td>Lorem, ipsum dolor.</td>
+						<td>Lorem.</td>
+					</tr>
+				</tbody>
+			</Table>
 		</>
 	);
 }
