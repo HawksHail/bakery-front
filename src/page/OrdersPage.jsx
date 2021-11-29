2c04dfcff164fe4eb1a4f51aa54c5072f2f5986a
Zion <zionman@verizon.net>
2021-11-29 15:05:35 -0600

    Implemented Order table rows
    Each order has
     * ID
     * date
     * status
     * total cost
     * a link to a details page

--- src/page/OrdersPage.jsx
+++ src/page/OrdersPage.jsx
@@ -1,27 +1,86 @@
 /* eslint-disable no-unused-vars */ //todo remove
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { Table } from "react-bootstrap";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Link } from "react-router-dom";
+
+import { getOrders } from "../api/orderAPI";
+import AppContext from "../contexts";
+import Loading from "../components/Loading";
 
 function OrderPage() {
+	const [orders, setOrders] = useState(null);
+	const { getAccessTokenSilently } = useAuth0();
+	const { customer } = useContext(AppContext);
+
+	useEffect(() => {
+		const abortController = new AbortController();
+		async function fetchOrders() {
+			const accessToken = await getAccessTokenSilently({
+				audience: "https://zion.ee-cognizantacademy.com",
+			});
+			getOrders(customer.customerId, accessToken).then(setOrders);
+		}
+		if (customer?.customerId) {
+			fetchOrders();
+		}
+		return () => {
+			abortController.abort(); // cancel pending fetch request on component unmount
+		};
+	}, [customer?.customerId, getAccessTokenSilently]);
+
+	console.log(`orders`, orders);
+
 	return (
 		<>
 			<h1>Orders</h1>
-			<Table striped bordered hover>
-				<thead>
-					<tr>
-						<th>Order Number</th>
-						<th>Order Date</th>
-						<th>Number of Items</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>Lorem, ipsum.</td>
-						<td>Lorem, ipsum dolor.</td>
-						<td>Lorem.</td>
-					</tr>
-				</tbody>
-			</Table>
+			{orders ? (
+				<Table striped bordered hover>
+					<thead>
+						<tr>
+							<th>Order Number</th>
+							<th>Date</th>
+							<th>Status</th>
+							<th>Total</th>
+							<th>Link</th>
+						</tr>
+					</thead>
+					<tbody>
+						{orders.map(order => (
+							<tr key={order.id}>
+								<td>
+									<Link to={`/orders/${order.id}`}>
+										#{order.id}
+									</Link>
+								</td>
+								<td>{order.orderDate}</td>
+								<td>Pending</td>
+								<td>
+									$
+									{order.detailsList
+										.reduce(
+											(prev, curr) =>
+												prev +
+												curr.product.unitPrice *
+													curr.quantity,
+											0
+										)
+										.toLocaleString(undefined, {
+											minimumFractionDigits: 2,
+										})}
+								</td>
+								<td>
+									<Link to={`/orders/${order.id}`}>View</Link>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</Table>
+			) : (
+				<h6>
+					<Loading />
+				</h6>
+			)}
 		</>
 	);
 }
