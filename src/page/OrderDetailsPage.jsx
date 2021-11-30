fda3bfa9783b53345576212f652d75e63b667bf5
Zion <zionman@verizon.net>
2021-11-30 10:25:56 -0600

    Added to OrderDetailsPage
     * Loading animation
     * Short sentence about order
     * API call to fetch data w/ abort

--- src/page/OrderDetailsPage.jsx
+++ src/page/OrderDetailsPage.jsx
@@ -1,13 +1,60 @@
 /* eslint-disable no-unused-vars */ //todo remove
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams, withRouter } from "react-router";
+import { useAuth0 } from "@auth0/auth0-react";
+
+import { getOrder } from "../api/orderAPI";
+import Loading from "../components/Loading";
 
 function OrderDetailsPage() {
 	const { id } = useParams();
+	const { getAccessTokenSilently } = useAuth0();
+	const [order, setOrder] = useState(null);
 
-	console.log(`id`, id);
+	useEffect(() => {
+		const abortController = new AbortController();
+		async function fetchOrder() {
+			const accessToken = await getAccessTokenSilently({
+				audience: "https://zion.ee-cognizantacademy.com",
+			});
+			getOrder(id, accessToken, abortController)
+				.then(setOrder)
+				.catch(console.log);
+		}
+		if (id) {
+			fetchOrder();
+		}
+		return () => {
+			abortController.abort(); // cancel pending fetch request on component unmount
+		};
+	}, [id, getAccessTokenSilently]);
 
-	return <h1>Order #{id}</h1>;
+	return (
+		<>
+			<h1>Order #{id}</h1>
+			{order ? (
+				<div>
+					<p>
+						Order{" "}
+						<span className="text-primary fw-bold">
+							#{order.id}
+						</span>{" "}
+						was placed on{" "}
+						<span className="text-primary fw-bold">
+							{order.orderDate}
+						</span>{" "}
+						and is currently{" "}
+						<span className="text-primary fw-bold">Processing</span>
+						.
+					</p>
+				</div>
+			) : (
+				<h4>
+					<Loading />
+				</h4>
+			)}
+		</>
+	);
 }
 
 export default withRouter(OrderDetailsPage);
--- src/tests/page/OrderDetailsPage.test.js
+++ src/tests/page/OrderDetailsPage.test.js
@@ -1,5 +1,9 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import {
+	render,
+	screen,
+	waitForElementToBeRemoved,
+} from "@testing-library/react";
 import nock from "nock";
 import { Route } from "react-router-dom";
 import { MemoryRouter } from "react-router";
@@ -9,6 +13,39 @@ import OrderDetailsPage from "../../page/OrderDetailsPage";
 import { url } from "../../api/url";
 
 jest.mock("@auth0/auth0-react");
+
+const fakeOrder = {
+	id: 1,
+	customer: {
+		customerId: 9,
+	},
+	orderDate: "2021-11-24",
+	detailsList: [
+		{
+			id: {
+				orderId: 1,
+				productId: 31,
+			},
+			product: {
+				id: 31,
+				productName: "Product31",
+				supplier: {
+					id: 1,
+					companyName: "Company1",
+					contactName: "Contact1",
+				},
+				category: {
+					id: 18,
+					categoryName: "Category18",
+					description: "Description18",
+				},
+				unitPrice: 8.5,
+				imgURL: "https://picsum.photos/100/100",
+			},
+			quantity: 2,
+		},
+	],
+};
 
 const user = {
 	email: "johndoe@me.com",
@@ -38,10 +75,14 @@ test("Renders title", () => {
 	nock(url)
 		.defaultReplyHeaders({
 			"Access-Control-Allow-Origin": "*",
+			"Access-Control-Allow-Headers": "Authorization",
 		})
+		.options("/order/1")
+		.optionally()
+		.reply(200)
 		.get("/order/1")
 		.optionally()
-		.reply(200);
+		.reply(200, fakeOrder);
 
 	render(
 		<MemoryRouter initialEntries={["/orders/1"]}>
@@ -52,4 +93,58 @@ test("Renders title", () => {
 	);
 
 	expect(screen.getByText(/order\s*#1/i)).toBeInTheDocument();
+});
+
+test("Renders Loading", () => {
+	nock(url)
+		.defaultReplyHeaders({
+			"Access-Control-Allow-Origin": "*",
+			"Access-Control-Allow-Headers": "Authorization",
+		})
+		.options("/order/1")
+		.optionally()
+		.reply(200)
+		.get("/order/1")
+		.optionally()
+		.reply(200, fakeOrder);
+
+	render(
+		<MemoryRouter initialEntries={["/orders/1"]}>
+			<Route path="/orders/:id">
+				<OrderDetailsPage />
+			</Route>
+		</MemoryRouter>
+	);
+
+	expect(screen.getByText(/^loading$/i)).toBeInTheDocument();
+});
+
+test("Calls API and renders order details", async () => {
+	nock(url)
+		.defaultReplyHeaders({
+			"Access-Control-Allow-Origin": "*",
+			"Access-Control-Allow-Headers": "Authorization",
+		})
+		.options("/order/1")
+		.optionally()
+		.reply(200)
+		.get("/order/1")
+		.optionally() //gets aborted and throws DOMException
+		.reply(200, fakeOrder);
+
+	render(
+		<MemoryRouter initialEntries={["/orders/1"]}>
+			<Route path="/orders/:id">
+				<OrderDetailsPage />
+			</Route>
+		</MemoryRouter>
+	);
+
+	await waitForElementToBeRemoved(screen.getByText(/^loading$/i));
+
+	expect(screen.getByText(/order #1/i)).toBeInTheDocument();
+	expect(screen.getByText(/was placed on/)).toBeInTheDocument();
+	expect(screen.getByText("2021-11-24")).toBeInTheDocument();
+	expect(screen.getByText(/and is currently/)).toBeInTheDocument();
+	expect(screen.getByText("Processing")).toBeInTheDocument();
 });
